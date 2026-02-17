3ba5aac709f87966d4974d4bb60a398a06e25774
0xnazrl <160778418+Alex000115@users.noreply.github.com>
2026-02-17 14:55:09 +0600

    Create StableToken.sol

---
+++ StableToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title StableToken
+ * @dev An ERC20 token with a mint/burn mechanism controlled by the Treasury.
+ */
+contract StableToken is ERC20, Ownable {
+    constructor() ERC20("AlgoUSD", "aUSD") Ownable(msg.sender) {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
